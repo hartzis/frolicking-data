f5190e753fd8b5323ced379c14a7ae31f5de58da
Brian Emil Hartz <brianhartz@gmail.com>
2015-06-14 11:15:47 -0600

    start gulp and client setup

---
+++ components/index.jsx
@@ -0,0 +1,17 @@
+let React = require('react');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Hello App
+      </div>
+    )
+  }
+
+}
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+let gulp = require('gulp');
+let browserify = require('browserify');
+let babelify = require('babelify');
+let source = require('vinyl-source-stream');
+let watchify = require('watchify');
+
+/* browserify */ 
+gulp.task('browserify', ()=>{
+  let bundler = browserify({
+    entries: ['./components/index.jsx'], // Only need initial file
+    transform: [babelify], // Convert JSX to javascript
+    debug: true, cache: {}, packageCache: {}, fullPaths: false
+  });
+
+  let watcher  = watchify(bundler);
+
+  return watcher
+  .on('update', ()=>{ // On update When any files updates
+    let updateStart = Date.now();
+        watcher.bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./client/js'));
+        console.log('Updated ', (Date.now() - updateStart) + 'ms');
+  })
+  .bundle() // Create initial bundle when starting the task 
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('./client/js'));
+});
+
+/* default */
+gulp.task('default', ['serve'], ()=>{});
+
+/* serve */
+gulp.task('serve', ['browserify'], ()=>{});
