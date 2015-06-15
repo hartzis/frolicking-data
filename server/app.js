45f503877857a43b9a29d9528db6f33fb6890443
Brian Emil Hartz <brianhartz@gmail.com>
2015-06-14 21:34:19 -0600

    add jade for templates

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
+// setup views and templates
 app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'jade');
 
+// serve js and css from "public" directory
 app.use(express.static(path.join(__dirname, '../client', 'public')));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index', { title: 'Hey', message: 'Hello there!'})
 });
 
 module.exports = app;
